5289499728905c34b143c438b12dbbc47326e5bd
hiratara <hiratara@cpan.org>
2011-02-26 07:48:10 +0900

    Add is_contents_ok() method.

--- main.py
+++ main.py
@@ -8,18 +8,21 @@ servers = [u"ブルー", u"オレンジ", u"グリーン"]
 pages   = [u"トップ", u"1", u"2", u"3", u"4", u"5"]
 
 class MainHandler(webapp.RequestHandler):
+    def is_contents_ok(self, content):
+        for server, page in ((s, p) for p in pages for s in servers):
+            if re.match(u"%s/%s" % (server, page), content): 
+                return False
+        return True
+
     def get(self):
         res = urllib2.urlopen("http://hiratara.dyndns.org:8080/chaberi/")
         content = "".join(res)
         if not content is unicode: content = content.decode("UTF-8")
+        if self.is_contents_ok(content):
+            self.response.out.write('OK')
+            return
 
-        fail = False
-        for server, page in ((s, p) for p in pages for s in servers):
-            if re.match(u"%s/%s" % (server, page), content): 
-                fail = True
-                break
-
-        self.response.out.write('Failed' if fail else 'OK')
+        self.response.out.write(fail)
 
 def main():
     application = webapp.WSGIApplication([('/', MainHandler)],
