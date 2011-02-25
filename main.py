0336635190bbf8ba808174f34063ce51e665c77b
hiratara <hiratara@cpan.org>
2011-02-26 08:40:34 +0900

    Fixed many many bugs and add models.

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 import urllib2
 import re
+from model import LastError
 
-servers = [u"ブルー", u"オレンジ", u"グリーン"]
-pages   = [u"トップ", u"1", u"2", u"3", u"4", u"5"]
+backdoor_url = "http://hiratara.dyndns.org:8080/chaberi/"
+# backdoor_url = "http://hiratara.dyndns.org:8080"
+servers = [ur"ブルー", ur"オレンジ", ur"グリーン"]
+pages   = [ur"トップ", ur"2", ur"3", ur"4", ur"5"]
+last_error_key = "lasterror"
 
 class MainHandler(webapp.RequestHandler):
     def is_contents_ok(self, content):
         for server, page in ((s, p) for p in pages for s in servers):
-            if re.match(u"%s/%s" % (server, page), content): 
+            if not re.search(ur"%s/%s" % (server, page), content): 
                 return False
         return True
 
     def get(self):
-        res = urllib2.urlopen("http://hiratara.dyndns.org:8080/chaberi/")
+        last_error = LastError.get_by_key_name(last_error_key)
+
+        res = urllib2.urlopen(backdoor_url)
         content = "".join(res)
         if not content is unicode: content = content.decode("UTF-8")
+
         if self.is_contents_ok(content):
-            self.response.out.write('OK')
+            if last_error:
+                last_error.delete()
+                message = 'recover'
+            else:
+                message = 'OK'
+
+            self.response.out.write(message)
             return
 
-        self.response.out.write(fail)
+        should_be_reported = True
+        if last_error:
+            should_be_reported = False
+        else:
+            LastError(key_name=last_error_key).put()
+
+        self.response.out.write('fail' if should_be_reported else 'no report')
 
 def main():
     application = webapp.WSGIApplication([('/', MainHandler)],
